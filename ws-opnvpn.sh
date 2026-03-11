fc8fbed1dd1c7ac707c6656ad6073e6c9b2ce937
Ajijainalmaster <arturrohim16@gmail.com>
2026-03-12 01:16:25 +0700

    Update ws-opnvpn.sh

--- ws-opnvpn.sh
+++ ws-opnvpn.sh
@@ -1,19 +1,21 @@
-#!/usr/bin/python5
+#!/usr/bin/python
 import socket, threading, thread, select, signal, sys, time, getopt
 
 # Listen
 LISTENING_ADDR = '0.0.0.0'
-LISTENING_PORT = sys.argv[1]
-
-# Pass
+if sys.argv[1:]:
+  LISTENING_PORT = sys.argv[1]
+else:
+  LISTENING_PORT = 2099  
+#Pass
 PASS = ''
 
 # CONST
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:1194'
-RESPONSE = 'HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\n\r\n'
-
+RESPONSE = 'HTTP/1.1 101 <u><font color="#00ff00">Switching Protocol</font></u>(Openvpn)\r\nContent-Length: 104857600000\r\n\r\n'
+RESPONSE = 'HTTP/1.1 200 Websocket_openvpn\r\nContent-length: 0\r\nHTTP/1.1 200 Connection established\r\n\r\n'
 
 class Server(threading.Thread):
     def __init__(self, host, port):
@@ -264,4 +266,3 @@ def main(host=LISTENING_ADDR, port=LISTENING_PORT):
 #######    parse_args(sys.argv[1:])
 if __name__ == '__main__':
     main()
-    
